75116606958b88432403cb0a33b71138a35cc8f7
seno-b <bangshinho@gmail.com>
2021-08-01 23:58:38 +0900

    feature: 4. API 개발 고급 - 컬렉션 조회 최적화 - 주문 조회 V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화 (2/2)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -52,13 +52,14 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
-        return em.createQuery(
+        List<Order> resultList = em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
                         " join fetch o.delivery d", Order.class)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
+        return resultList;
     }
 
 }
